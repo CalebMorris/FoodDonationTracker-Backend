0ae24f489fba451b257f861f4399abc8e5a14efa
Caleb Morris <caleb.morris.g@gmail.com>
2013-02-19 21:02:35 -0700

    json formated similar to tested php file

--- front_end/front_end/Pusher.cs
+++ front_end/front_end/Pusher.cs
@@ -13,16 +13,26 @@ namespace front_end
 			deviceId = "APA91bF7QrhCcjwXKNiXB9HOMnBJA8CEdWemgPCzEl7j5Uvd0RQrIs4iAF2oPE4houHibikUs8Mvgb0Dvt_RgWcOcRsMnI2kpgTdnL1iAtnqFSkH69Ryj4X71dBQ-MOPZZkg4iXzuc99-oHO-PEoanNlNl7Oh3zuAA";
 			message = "test";
 			string GoogleAppID = "AIzaSyBlcyCnxYJNfk5mF4sfzcOt6q2j5S_wQRI";
-			var SENDER_ID = "89910554410";
+			//var SENDER_ID = "89910554410";
 			var value = message;
 			WebRequest tRequest;
 			tRequest = WebRequest.Create("https://android.googleapis.com/gcm/send");
 			tRequest.Method = "post";
-			tRequest.ContentType = " application/x-www-form-urlencoded;charset=UTF-8";
+			tRequest.ContentType = " application/json";
 			tRequest.Headers.Add(string.Format("Authorization: key={0}", GoogleAppID));
-			tRequest.Headers.Add(string.Format("Sender: id={0}", SENDER_ID));
+			//tRequest.Headers.Add(string.Format("Sender: id={0}", SENDER_ID));
 				
-			string postData = "collapse_key=score_update&time_to_live=108&delay_while_idle=1&data.message=" + value + "&data.time=" + System.DateTime.Now.ToString() + "®istration_id=" + deviceId + "";
+			string postData = "{"
+					+ "\"registration_id\":\"" + deviceId + "\"" + ","
+					+"\"data\":{"
+						+ "\"time\":\"" 
+						+ System.DateTime.Now.ToString() + "\"" + ","
+						+ "\"message\":\""
+						+ value + "\"" + ","
+						+ "\"status\":\""
+						+ "assigned" + "\""
+					+"}"
+				+"}";
 			Console.WriteLine(postData);
 			Byte[] byteArray = Encoding.UTF8.GetBytes(postData);
 			tRequest.ContentLength = byteArray.Length;
