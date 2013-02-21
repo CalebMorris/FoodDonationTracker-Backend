10fc5cff0679d985f6c66a2b6e70e1bd1afdff9c
Caleb Morris <caleb.morris.g@gmail.com>
2013-02-20 23:31:20 -0700

    transfer doesn't like doubles

--- front_end/front_end/GPS.cs
+++ front_end/front_end/GPS.cs
@@ -11,6 +11,10 @@ namespace back_end
 		public GPS ( double lat, double lon ) {
 			latitude = lat; longitude = lon;
 		}
+		public GPS ( int lat, int lon ) {
+			this.latitude = (int)(lat*1000000);
+			this.longitude = (int)(lon*1000000);
+		}
 		
 		public double getLat() { return latitude; }
 		public double getLon() { return longitude; }
