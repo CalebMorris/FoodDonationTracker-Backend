66dc7f97d1e9422f887d1033a23722f93daa82fb
Caleb Morris <caleb.morris.g@gmail.com>
2013-02-20 23:33:17 -0700

    limit to testing just one

--- front_end/front_end/Pusher.cs
+++ front_end/front_end/Pusher.cs
@@ -11,9 +11,6 @@ namespace front_end
 		
 		public static string SendNotification(string deviceId, string message) {
 			deviceId = "APA91bF7QrhCcjwXKNiXB9HOMnBJA8CEdWemgPCzEl7j5Uvd0RQrIs4iAF2oPE4houHibikUs8Mvgb0Dvt_RgWcOcRsMnI2kpgTdnL1iAtnqFSkH69Ryj4X71dBQ-MOPZZkg4iXzuc99-oHO-PEoanNlNl7Oh3zuAA";
-			string Id2 = "APA91bFhnzVwJ6bKx_GT2BysxMrH4IyCa6je0w2rJ12tpO8RWl2KOhiR6vBrVC7g0JKcrJw_mAPKCXmseM2bgPG5W88-NIfayPIsypD2y44H9IH7ut4mZBs6FGkUeSNWcyD76xfznQckTIHkQtng6cdL43I1pxBd9w";
-			string Id3 = "APA91bFPolfZFHVGV5NgAftyN_rqFcbswi7N9DRHNmsAcG7T6zuUCq7bgH42-UIUbStPS41jWH15YZPdtoHs6W9l_YZBdAV5eS_J_n2TOy-7jj90wCn2DdOGNrZ6yaDjvtafBpmNuLHv2-TsRgLmUYESxXi14Ta0BQ";
-			string Id4 = "APA91bEshtaAMt7lsOG428cLEm0oDL4vQwqMlDr5WkN4eGBi6V-6RrGnNYRRacwpvkqLmw5xfZoLXOxn9oPcDnvJo-IN8xzpjyuPfZcvgCWycw79lDBa06IF_2qSsNnORok0iotdsCD1cqrLrtP7KiEKsHsx3sDj1w";
 			message = "test";
 			string GoogleAppID = "AIzaSyBlcyCnxYJNfk5mF4sfzcOt6q2j5S_wQRI";
 			//var SENDER_ID = "89910554410";
@@ -26,10 +23,7 @@ namespace front_end
 			//tRequest.Headers.Add(string.Format("Sender: id={0}", SENDER_ID));
 				
 			string postData = "{"
-					+ "\"registration_ids\":[" + "\"" + deviceId + "\"" + ","
-						+ "\"" + Id2 + "\"" + ","
-						+ "\"" + Id3 + "\"" + ","
-						+ "\"" + Id4 + "\""
+					+ "\"registration_ids\":[" + "\"" + deviceId + "\""
 					+ "]"
 					+"\"data\":{"
 						+ "\"time\":\"" 
