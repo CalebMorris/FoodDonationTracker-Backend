fddc9191a0743ff496838edae68c14015b4543c3
Caleb Morris <caleb.morris.g@gmail.com>
2013-04-20 22:17:17 -0700

    altered to use hash function

--- front_end/front_end/Global.asax.cs
+++ front_end/front_end/Global.asax.cs
@@ -14,11 +14,11 @@ namespace front_end
 		protected virtual void Application_Start (Object sender, EventArgs e)
 		{
 			Application["donors"] = new List<Donor>();
-			((List<Donor>)Application["donors"]).Add(new Donor("donor","1a5e9276c3085f0f612a9b8332849aa7aaaa",new GPS(033419084, -111938109), 100));
+			((List<Donor>)Application["donors"]).Add(new Donor("donor",webservice_mono.saltPass("pass"),new GPS(033419084, -111938109), 100));
 			Application["receivers"] = new List<Receiver>();
-			((List<Receiver>)Application["receivers"]).Add(new Receiver("receiver","1a5e9276c3085f0f612a9b8332849aa7aaaa",new GPS(033419505, -111912800),"","",""));
+			((List<Receiver>)Application["receivers"]).Add(new Receiver("receiver",webservice_mono.saltPass("pass"),new GPS(033419505, -111912800),"","",""));
 			Application["drivers"] = new List<Driver>();
-			((List<Driver>)Application["drivers"]).Add(new Driver("driver","1a5e9276c3085f0f612a9b8332849aa7aaaa", new GPS(1,1)));
+			((List<Driver>)Application["drivers"]).Add(new Driver("driver",webservice_mono.saltPass("pass"), new GPS(1,1)));
 			Application["users"] = new Dictionary<String, Tuple<User, String>>();
 			Application["activeDonations"] = new List<Donor>();
 			Application["queue"] = new Queue_t<Donation>();
